5f371be6361367a5ae669c179d1a8d23e8e8c507
eluharani yassine <eluharani.yassine@gmail.com>
2023-03-20 13:45:30 +0000

    fixed auto formatting

--- after/plugin/lsp/null-ls.lua
+++ after/plugin/lsp/null-ls.lua
@@ -8,7 +8,6 @@ end
 local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
--- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
@@ -27,22 +26,19 @@ null_ls.setup({
     }),
   },
   -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+  -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({bufnr = bufnr})
+                end,
+            })
+        end
+    end,
 })
+
