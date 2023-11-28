8d030c6bfd36ce191274945ef093f22e1004444b
Yassine Eluharani <eluharani.yassine@gmail.com>
2023-11-28 12:57:38 +0100

    Lazy package manager.

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -11,7 +11,6 @@ require("nvim-treesitter.configs").setup({
 		"yaml",
 		"html",
 		"css",
-		"markdown",
 		"graphql",
 		"bash",
 		"dockerfile",
--- nvim/lua/levisine/init.lua
+++ nvim/lua/levisine/init.lua
@@ -1,3 +1,196 @@
 require("levisine.remap")
 require("levisine.set")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ","
+
+require("lazy").setup({
+
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "wittyjudge/gruvbox-material.nvim", name = "gruvbox-material" },
+	"nvim-treesitter/nvim-treesitter",
+	"ThePrimeagen/harpoon",
+	"mbbill/undotree",
+	"tpope/vim-surround",
+	-- Comment,
+	"numToStr/Comment.nvim",
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	"nvim-lualine/lualine.nvim",
+	-- autocompletion,
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	--Snippets,
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+	--Formatting & Linting,
+	"jose-elias-alvarez/null-ls.nvim",
+	"jayp0521/mason-null-ls.nvim",
+	-- mason lsp configuration,
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	-- Allows us to configure so they appear in autpcomplition,
+	"hrsh7th/cmp-nvim-lsp",
+	-- enhanced UI to our lsp,
+	-- added functionalities for tsserver,
+	"jose-elias-alvarez/typescript.nvim",
+	-- icons for autocomplition,
+	"onsails/lspkind.nvim",
+	--Auto closing,
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	--Git integration,
+	"lewis6991/gitsigns.nvim",
+	"christoomey/vim-tmux-navigator",
+	"szw/vim-maximizer",
+	"kdheepak/lazygit.nvim",
+	{
+		lazy = true,
+		"AckslD/nvim-neoclip.lua",
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" },
+			-- {'ibhagwan/fzf-lua'},
+		},
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
+	{
+		lazy = true,
+		"folke/trouble.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		lazy = true,
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	},
+	{
+		lazy = true,
+		"jackMort/ChatGPT.nvim",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
+		"Wansmer/treesj",
+		config = function()
+			require("treesj").setup()
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		require("gitblame").setup({
+			enabled = false,
+		}),
+	},
+	"xiyaowong/transparent.nvim",
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+	},
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss all Notifications",
+			},
+		},
+		opts = {
+			background_colour = "#000000",
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+		},
+	},
+	"folke/noice.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	{
+		lazy = true,
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"haydenmeade/neotest-jest",
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			plugins = { spelling = true },
+			defaults = {
+				mode = { "n", "v" },
+				["g"] = { name = "+goto" },
+				["gs"] = { name = "+surround" },
+				[")"] = { name = "+next" },
+				["("] = { name = "+prev" },
+				["<leader><tab>"] = { name = "+tabs" },
+				["<leader>b"] = { name = "+buffer" },
+				["<leader>c"] = { name = "+code" },
+				["<leader>f"] = { name = "+file/find" },
+				["<leader>g"] = { name = "+git" },
+				["<leader>gh"] = { name = "+hunks" },
+				["<leader>q"] = { name = "+quit/session" },
+				["<leader>s"] = { name = "+search" },
+				["<leader>u"] = { name = "+ui" },
+				["<leader>w"] = { name = "+windows" },
+				["<leader>x"] = { name = "+diagnostics/quickfix" },
+			},
+		},
+	},
+})
