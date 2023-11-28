ff7c0a8bb75ea00655e9b1614e4f9fd5bbd1010c
Yassine Eluharani <eluharani.yassine@gmail.com>
2023-11-28 14:44:27 +0100

    Git blame with gitSigns plugin

--- nvim/lua/levisine/init.lua
+++ nvim/lua/levisine/init.lua
@@ -61,7 +61,20 @@ require("lazy").setup({
 	"windwp/nvim-autopairs",
 	"windwp/nvim-ts-autotag",
 	--Git integration,
-	"lewis6991/gitsigns.nvim",
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 500,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+		},
+	},
 	"christoomey/vim-tmux-navigator",
 	"szw/vim-maximizer",
 	"kdheepak/lazygit.nvim",
@@ -70,7 +83,6 @@ require("lazy").setup({
 		"AckslD/nvim-neoclip.lua",
 		dependencies = {
 			{ "nvim-telescope/telescope.nvim" },
-			-- {'ibhagwan/fzf-lua'},
 		},
 		config = function()
 			require("neoclip").setup()
